ba46552d11019f23d0c9d078cf124f489d6f18e6
rrohit2901 <rrohit2901@gmail.com>
2020-04-13 04:49:49 +0530

    Used labels to be used by classifier

--- Testing_annotation using surf/surf.py
+++ Testing_annotation using surf/surf.py
@@ -9,28 +9,23 @@ surf = cv2.xfeatures2d.SURF_create(7000)
 key_points, descriptors = surf.detectAndCompute(img,None)
 # Manually figured out corner points 
 dim = descriptors.shape
-key_points_filtered = []
-key_points_filtered.append(key_points[0])
-key_points_filtered.append(key_points[2])
-key_points_filtered.append(key_points[5])
-key_points_filtered.append(key_points[9])
-descriptors_f = []
-descriptors_f.append(descriptors[0])
-descriptors_f.append(descriptors[2])
-descriptors_f.append(descriptors[5])
-descriptors_f.append(descriptors[9])
-f_data = []
+corner = [0,2,5,9]
 # Writing data in csv file
-for i in range(len(key_points_filtered)):
+f_data = []
+for i in range(len(key_points)):
     temp = []
-    temp.append(key_points_filtered[i])
+    if i in corner:
+        temp.append(1)
+    else:
+        temp.append(0)
+    temp.append(key_points[i])
     for j in range(dim[1]):
-        temp.append(descriptors_f[i][j])
+        temp.append(descriptors[i][j])
     f_data.append(temp)
 with open('information.csv','w',newline = '')  as file:
     writer = csv.writer(file)
     writer.writerows(f_data)
 # Drawing points
-cv2.drawKeypoints(img, key_points_filtered, out_img, (0,0,0), flags = cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
+cv2.drawKeypoints(img, key_points, out_img, (0,0,0), flags = cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
 cv2.imshow("window", out_img)
 cv2.waitKey(10000)
