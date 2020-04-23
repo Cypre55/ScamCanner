3d0453c7d19808ddd4b89a1c19a44ebff6ea4771
rrohit2901 <rrohit2901@gmail.com>
2020-04-23 12:33:53 +0530

    Used k means clustering for corner detection

--- logistic_regression/log3.py
+++ logistic_regression/log3.py
@@ -4,6 +4,7 @@ import cv2
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn import svm
+from sklearn.cluster import KMeans
 from scipy.stats import boxcox
 import imblearn
 from sklearn import preprocessing, metrics
@@ -49,7 +50,6 @@ for file in os.listdir(inputDir):
         key_points_e = []
         key_points_ne = []
         descriptors = pd.DataFrame(descriptors)
-
         a = log_reg.predict(descriptors)
         for i in range(len(a)):
             if a[i]==1:
@@ -57,8 +57,31 @@ for file in os.listdir(inputDir):
             else:
                 key_points_ne.append(key_points[i])
 
+        cor = []
+        for i in range(len(key_points_e)):
+            temp = []
+            temp.append(int((key_points_e[i]).pt[0]))
+            temp.append(int((key_points_e[i]).pt[1]))
+            cor.append(temp)
+        cor = np.asarray(cor)
+        kmeans = KMeans(n_clusters = 4, n_init = 50).fit(cor)
+        cor_cent = kmeans.cluster_centers_
+        box = []
+        for i in range(len(cor_cent)):
+            temp = []
+            temp.append(int(cor_cent[i][0]))
+            temp.append(int(cor_cent[i][1]))
+            temp = np.asarray(temp)
+            temp1 = []
+            temp1.append(temp)
+            temp1 = np.asarray(temp1)
+            box.append(temp)
+        box = np.asarray(box)
+        hull = []
+        hull.append(cv2.convexHull(box,False))
         out_img = np.copy(org_img)
         cv2.drawKeypoints(org_img, key_points_e, out_img, (0,0,255), flags = cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
         cv2.drawKeypoints(org_img, key_points_ne, out_img, (255,0,0), flags = cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
+        cv2.drawContours(out_img, hull, 0, (0,0,255),8)
         cv2.imshow("window", out_img)
         cv2.waitKey(10000)
