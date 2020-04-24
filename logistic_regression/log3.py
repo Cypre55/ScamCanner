7c11eb16b6a3cc272b24bad7329fdd3d51318d1f
Agni Purani <agni.purani@gmail.com>
2020-04-24 22:01:24 +0530

    Completed simple application

--- logistic_regression/log3.py
+++ logistic_regression/log3.py
@@ -12,14 +12,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os, subprocess
 import math
+import joblib
+import imutils
+from transform import make_document
 
-l = range(2,66)
-y_df = pd.read_csv('data.csv', delimiter = ',', header = None, usecols = [0])
-x_df = pd.read_csv('data.csv', delimiter = ',', header = None, usecols = l)
-x_normalized = preprocessing.normalize(x_df, norm='l2')
-y_df = np.array(y_df[0])
-log_reg = LogisticRegression(solver = 'saga', max_iter = 500, C = 100.0)
-log_reg.fit(x_df, y_df)
+try:
+    log_reg = joblib.load('trainned.pkl')
+except:
+    l = range(2,66)
+    y_df = pd.read_csv('data.csv', delimiter = ',', header = None, usecols = [0])
+    x_df = pd.read_csv('data.csv', delimiter = ',', header = None, usecols = l)
+    x_normalized = preprocessing.normalize(x_df, norm='l2')
+    y_df = np.array(y_df[0])
+    log_reg = LogisticRegression(solver = 'saga', max_iter = 500, C = 100.0)
+    log_reg.fit(x_df, y_df)
+    joblib.dump(log_reg, 'trainned.pkl')
 
 inputDir = "ImageData"
 
@@ -30,6 +37,7 @@ for file in os.listdir(inputDir):
         img_path = inputDir + '/' + file
         img1 = cv2.imread(img_path,1)
         org_img = img1.copy()
+        org = img1.copy()
         img1 = cv2.medianBlur(img1,29)
         kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
         img1 = cv2.filter2D(img1, -1, kernel)
@@ -64,24 +72,31 @@ for file in os.listdir(inputDir):
             temp.append(int((key_points_e[i]).pt[1]))
             cor.append(temp)
         cor = np.asarray(cor)
-        kmeans = KMeans(n_clusters = 4, n_init = 50).fit(cor)
-        cor_cent = kmeans.cluster_centers_
-        box = []
-        for i in range(len(cor_cent)):
-            temp = []
-            temp.append(int(cor_cent[i][0]))
-            temp.append(int(cor_cent[i][1]))
-            temp = np.asarray(temp)
-            temp1 = []
-            temp1.append(temp)
-            temp1 = np.asarray(temp1)
-            box.append(temp)
-        box = np.asarray(box)
-        hull = []
-        hull.append(cv2.convexHull(box,False))
-        out_img = np.copy(org_img)
-        cv2.drawKeypoints(org_img, key_points_e, out_img, (0,0,255), flags = cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
-        cv2.drawKeypoints(org_img, key_points_ne, out_img, (255,0,0), flags = cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
-        cv2.drawContours(out_img, hull, 0, (0,0,255),8)
-        cv2.imshow("window", out_img)
-        cv2.waitKey(10000)
+        try:
+            kmeans = KMeans(n_clusters = 4, n_init = 15).fit(cor)
+            cor_cent = kmeans.cluster_centers_
+            box = []
+            for i in range(len(cor_cent)):
+                temp = []
+                temp.append(int(cor_cent[i][0]))
+                temp.append(int(cor_cent[i][1]))
+                temp = np.asarray(temp)
+                temp1 = []
+                temp1.append(temp)
+                temp1 = np.asarray(temp1)
+                box.append(temp)
+            box = np.asarray(box)
+            hull = []
+            hull.append(cv2.convexHull(box,False))
+            out_img = np.copy(org_img)
+            cv2.drawKeypoints(org_img, key_points_e, out_img, (0,0,255), flags = cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
+            cv2.drawKeypoints(org_img, key_points_ne, out_img, (255,0,0), flags = cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
+            cv2.drawContours(out_img, hull, 0, (0,0,255),8)
+            
+            warped = make_document(org, hull, 4)
+
+            cv2.imshow("doc", warped)
+            cv2.imshow("window", out_img)
+            cv2.waitKey(0)
+        except:
+            print("No document found!")
